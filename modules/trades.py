973225ce40a6c54852be41e8544986b39a15bf3a
Anodynous <anodynestrike@gmail.com>
2016-08-27 03:01:42 +0300

    shared formatting for buy and sell output

--- modules/trades.py
+++ modules/trades.py
@@ -11,32 +11,25 @@ def trades(minsize=0, coin_pair='BTCUSD'):  # prints trades equal to or larger t
         try:
             if result[1] == 'te':
                 if abs(float(result[5])) > float(minsize):
+                    trd_f = [32, 42]
                     if result[5] > 0:
                         trd_direction = 'BUY: '
                     else:
                         trd_direction = 'SELL:'
+                        trd_f = [x - 1 for x in trd_f]
+                    result_abs = abs(result[5])
                     result_timestamp = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-                    result_format = (str(round(abs(result[5]), 5)).ljust(7),str(result[4]).rjust(10), result_timestamp.rjust(16))
-                    if float(result[5]) > 100:
-                        print('\033[1;37;42mBUY:  {0} {1}\033[0m {2}'.format(*result_format))
-                    elif float(result[5]) > 50:
-                        print('\033[1;37;42mBUY:\033[1;32;0m\033[0;32;32m  {0} {1}\033[0m {2}'.format(*result_format))
-                    elif float(result[5]) > 10:
-                        print('\033[1;32;32mBUY:  {0} {1}\033[0m {2}'.format(*result_format))
-                    elif float(result[5]) > 1:
-                        print('\033[0;32;32mBUY:  {0} {1}\033[0m {2}'.format(*result_format))
-                    elif float(result[5]) > 0:
-                        print('\033[0;32;32mBUY:\033[0m  {0} {1} {2}'.format(*result_format))
-                    elif float(result[5]) < -100:
-                        print('\033[1;37;41mSELL: {0} {1}\033[0m {2}'.format(*result_format))
-                    elif float(result[5]) < -50:
-                        print('\033[1;37;41mSELL:\033[1;31;0m\033[0;31;31m {0} {1}\033[0m {2}'.format(*result_format))
-                    elif float(result[5]) < -10:
-                        print('\033[1;31;31mSELL: {0} {1}\033[0m {2}'.format(*result_format))
-                    elif float(result[5]) < -1:
-                        print('\033[0;31;31mSELL: {0} {1}\033[0m {2}'.format(*result_format))
-                    elif float(result[5]) < 0:
-                        print('\033[0;31;31mSELL:\033[0m {0} {1} {2}'.format(*result_format))
+                    result_format = (trd_direction, str(round(abs(result[5]), 5)).ljust(7), str(result[4]).rjust(10), result_timestamp.rjust(16))
+                    if result_abs >= 100:
+                        print('\033[1;37;'+ str(trd_f[1]) + 'm{0} {1} {2}\033[0m {3}'.format(*result_format))
+                    elif result_abs >= 50:
+                        print('\033[1;37;'+ str(trd_f[1]) + 'm{0}\033[1;'.format(trd_direction)+ str(trd_f[0]) + ';0m\033[0;'+ str(trd_f[0]) + ';'+ str(trd_f[0]) + 'm {1} {2}\033[0m {3}'.format(*result_format))
+                    elif result_abs >= 10:
+                        print('\033[1;'+ str(trd_f[0]) + ';'+ str(trd_f[0]) + 'm{0} {1} {2}\033[0m {3}'.format(*result_format))
+                    elif result_abs >= 1:
+                        print('\033[0;'+ str(trd_f[0]) + ';'+ str(trd_f[0]) + 'm{0} {1} {2}\033[0m {3}'.format(*result_format))
+                    elif result_abs < 1:
+                        print('\033[0;'+ str(trd_f[0]) + ';'+ str(trd_f[0]) + 'm{0}\033[0m {1} {2} {3}'.format(*result_format))
         except:
             if result['event'] == 'subscribed':
                 print('Subscribed to {0} at {1}'.format(result['pair'], datetime.now().strftime('%H:%M:%S')))
