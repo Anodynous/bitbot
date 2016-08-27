f07ffb1e43bbe9e3409eca7fc3f7a0ab9344af4b
Anodynous <anodynestrike@gmail.com>
2016-08-27 14:18:14 +0300

    compatible with general output method. Fixes.

--- modules/orderbook.py
+++ modules/orderbook.py
@@ -6,14 +6,15 @@ from operator import itemgetter
 
 from modules.connect import bitfinexConnect
 from modules.price import current_price
+from modules.output import output
 
-
-def order_book(ordertype, price_range=0):  # prints order book data
+def order_book(ordertype, price_range=0):  # outputs order book data
     if price_range != 0:
         last_price = float(current_price())
     r = requests.get('https://api.bitfinex.com/v1/book/BTCUSD')
     parsed_json = json.loads(r.text)
     book_usd_value = 0
+    book_btc_value = 0
     n = 0
     for i in range(0, 5000):
         try:
@@ -25,11 +26,15 @@ def order_book(ordertype, price_range=0):  # prints order book data
                 btc_amount = parsed_json[ordertype][i]['amount']
                 usd_value = float(btc_amount) * float(usd_price)
                 book_usd_value += usd_value
-                print(str(btc_amount) + ' BTC @ ' + str(usd_price) + ' USD. Placed: ' + str(
+                book_btc_value += float(btc_amount)
+                output(str(btc_amount) + ' BTC @ ' + str(usd_price) + ' USD. Placed: ' + str(
                     timestamp) + ' Valued: ' + str(usd_value) + ' USD')
         except:
-            print('\n\n', (n + 1), ' ', ordertype, ' orders')
-            print('Combined value of: {:,}'.format(int(book_usd_value)), ' USD')
+            cumulative_usd = "{:,}".format(int(book_usd_value))
+            cumulative_btc = "{:,}".format(int(book_btc_value))
+            output('\n\n{0} {1} {2}'.format((n + 1), ordertype[:-1], ' orders'))
+            output('Cumulative value of: {0} {1}'.format(cumulative_usd, 'USD'))
+            output('Cumulative quantity of: {0} {1}'.format(cumulative_btc, 'BTC'))
             sys.exit(1)
 
 
@@ -46,18 +51,18 @@ def raw_order_book():
                     if result[1] == order_book[n][0]:
                         if result[3] != order_book[n][2]:
                             if result[2] != 0:
-                                print('UPDATE: , [{0}, {1}, {2}] :: {3}'.format(result[1], result[2], result[3], order_book[n]))
+                                output(('UPDATE: , [{0}, {1}, {2}] :: {3}'.format(result[1], result[2], result[3], order_book[n])))
                                 order_book[n] = [result[1], result[2], result[3]]
                                 break
                             else:
                                 del order_book[n]
-                                print('removed! book lenght now:', len(order_book))
+                                output(('removed! book lenght now:', len(order_book)))
                         elif result[3] == order_book[n][2]:
                             break
                     elif n >= (len(order_book)-2) and result[2] != 0:
                         new_bookpoint = [result[1], result[2], result[3]]
                         order_book.append(new_bookpoint)
-                        print('APPEND: , [{0}, {1}, {2}]'.format(result[1], result[2], result[3]))
+                        output(('APPEND: , [{0}, {1}, {2}]'.format(result[1], result[2], result[3])))
             elif len(result) == 2 and result[1] != 'hb':
                 order_book = result[1]
                 order_book = sorted(order_book, key = itemgetter(0), reverse = True)
