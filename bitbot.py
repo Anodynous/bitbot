3824a5bf53ea21608b40c125a07a4587816f4896
Anodynous <anodynestrike@gmail.com>
2016-08-29 20:39:35 +0300

    initial move to argparse

--- bitbot.py
+++ bitbot.py
@@ -1,60 +1,43 @@
 #!/usr/bin/env python3
 import sys
+import argparse
 
 from modules.logger import trade_logger
 from modules.orderbook import order_book
 from modules.orderbook import raw_order_book
 from modules.trades import trades
 from modules.price import ticker
-from modules.output import output
 
 if sys.version_info[0] < 3:
-        raise "Requires Python 3+"
+    sys.stdout.write('Sorry, requires Python 3+')
+    sys.exit(1)
 
 
-def main():  # main function
-    if len(sys.argv) > 1:
-        if sys.argv[1] == '-h':
-            output('options:\n-trades (coin_pair minsize)\n-ticker\n-log\n-raw\n-orderbook (asks/bids range)')
-        elif sys.argv[1] == '-trades':
-            if len(sys.argv) > 2:
-                try:
-                    trades(sys.argv[3], sys.argv[2])
-                except:
-                    trades(coin_pair=sys.argv[2])
-            elif len(sys.argv) > 1:
-                try:
-                    trades(sys.argv[2])
-                except:
-                    trades()
-        elif sys.argv[1] == '-ticker':
-            ticker()
-        elif sys.argv[1] == '-log':
-            trade_logger()
-        elif sys.argv[1] == '-raw':
-            raw_order_book()
-        elif sys.argv[1] == '-orderbook':
-            if len(sys.argv) > 2:
-                if sys.argv[2] in ('asks', 'bids'):
-                    if len(sys.argv) > 3:
-                        if 0 <= int(sys.argv[3]) <= 1000:
-                            order_book(sys.argv[2], sys.argv[3])
-                        else:
-                            output('check range attribute')
-                    else:
-                        order_book(sys.argv[2])
-                else:
-                    output("please define either 'asks' or 'bids'")
-            else:
-                output("please define either 'asks' or 'bids'")
-        else:
-            output('check command line arguments')
-    else:
-        output('no command line arguments provided')
+def main():
+    parser = argparse.ArgumentParser(description='Command line utility for Bitfinex API')
+    group = parser.add_mutually_exclusive_group()
+    parser.add_argument('minsize', metavar='size', type=float, help='minimum trade size or USD amount (orderbook)', nargs='?', default=0)
+    parser.set_defaults(minsize=0)
+    group.add_argument('-t', '--trades', help='live trade feed', action='store_true')
+    group.add_argument('-ti', '--ticker', help='price ticker', action='store_true')
+    group.add_argument('-l', '--log', help='log raw trade feed', action='store_true')
+    group.add_argument('-o', '--orderbook', type=str, choices=['bid', 'ask', 'all'], default='bids', help='display orderbook', action='store')
+    group.add_argument('-r', '--rawbook', help='show raw orderbook feed', action='store_true')
+    args = parser.parse_args()
+    if args.trades:
+        trades(minsize=args.minsize)
+    elif args.ticker:
+        ticker()
+    elif args.log:
+        trade_logger()
+    elif args.rawbook:
+        raw_order_book()
+    elif args.orderbook:
+        order_book(ordertype=args.orderbook, price_range=args.minsize)
 
 
 if __name__ == "__main__":
     try:
         main()
-    except KeyboardInterrupt:   # cleaner exit without showing traceback
+    except KeyboardInterrupt:   # cleaner exit without printing traceback on ctr+c
         pass
